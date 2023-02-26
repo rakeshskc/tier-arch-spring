f3d414d91f1a16a75f79acf3719a1e69f3647518
rakeshskc <rakeshchaudhari1111@gmail.com>
2023-02-26 16:20:28 +0530

    Log4j2 Added

--- sampleapp/src/main/java/com/skc/demo/SampleAppApplication.java
+++ sampleapp/src/main/java/com/skc/demo/SampleAppApplication.java
@@ -1,15 +1,22 @@
 package com.skc.demo;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.skc"})
+@ComponentScan(basePackages = { "com.skc" })
 public class SampleAppApplication {
+	
+	private static Logger logger = LogManager.getLogger(SampleAppApplication.class);
 
 	public static void main(String[] args) {
+		logger.debug("App Service Starting");
 		SpringApplication.run(SampleAppApplication.class, args);
 	}
-
+	
 }
--- sampleweb/src/main/java/com/skc/demo/SampleWebApplication.java
+++ sampleweb/src/main/java/com/skc/demo/SampleWebApplication.java
@@ -1,5 +1,7 @@
 package com.skc.demo;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -8,10 +10,13 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.skc","com.skc.controller"})
+@ComponentScan(basePackages = {"com.skc"})
 public class SampleWebApplication extends SpringBootServletInitializer {
 
+	private static Logger logger = LogManager.getLogger(SampleWebApplication.class);
+	
 	public static void main(String[] args) {
+		logger.debug("Web Service Starting");
 		SpringApplication.run(SampleWebApplication.class, args);
 	}
 	
